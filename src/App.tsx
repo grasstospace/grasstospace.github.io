ff38473557962b7a3d4d9a2c7543a6d9652bf7c8
Adit Gupta <guptaditz@gmail.com>
2025-12-05 09:02:54 -0800

    Initialized website with Vite

--- src/App.tsx
+++ src/App.tsx
@@ -16,17 +16,14 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Grass to Space Website</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Nothing here yet, this is the Grass to Space ARC team.
       </p>
     </>
   )

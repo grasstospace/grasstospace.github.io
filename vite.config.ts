4253e62c0dc38a2b132fc19e1cc5cfd899ce89ed
Adit Gupta <guptaditz@gmail.com>
2026-02-10 23:52:04 -0800

    changed base to / instead of /ARC-team

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ARC-team/',
+  base: '/',
   build: {
     outDir: 'docs',
     emptyOutDir: true,
